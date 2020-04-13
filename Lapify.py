99e38a8214731f0906207162a6001ead9a6b8040
Mixeroowsky <33137122+Mixeroowsky@users.noreply.github.com>
2020-04-13 20:28:56 +0200

    Add logos and example utilities to popup

--- Lapify.py
+++ Lapify.py
@@ -1,24 +1,44 @@
+import kivy
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
+import kivy.utils
 
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '1280')
 Config.set('graphics', 'height', '720')
 
 
-class StackLayout(StackLayout):
-    pass
+class Stack(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def btn(self):
+        show_popup()
+
+
 class TitleBar(Widget):
     pass
+
+
+class PopLay(FloatLayout):
+    pass
+
 
 class LapifyApp(App):
     def build(self):
         lapi = TitleBar()
         return lapi
+
+
+def show_popup():
+    show = PopLay()
+    popupWindow = Popup(title="Rozpocznij nowa sesje", content=show, size_hint=(.7, .7), auto_dismiss=False)
+    popupWindow.open()
+
 
 if __name__ == '__main__':
     LapifyApp().run()
